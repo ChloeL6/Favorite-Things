202dda90b3bfb5774933e6f6584839185b248c7b
Chloe Le <yenchi.le1199@gmail.com>
2022-10-24 15:04:58 -0700

    Add index.html and scripts
    Co-authored-by: Dulce Jaramillo<dulce.jaramillo@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function() {
+const first = document.getElementById("first").value;
+const second = document.getElementById("second").value;
+const last = document.getElementById("last").value;
+
+const array = (first, second, last);
+
+
+
+});
